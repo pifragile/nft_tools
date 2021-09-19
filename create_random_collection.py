943ff6e9349131b4de6428590cd7802c069ba647
pifragile <pigu@bluewin.ch>
2021-09-19 11:49:58 +0200

    optimize argument parsing in collection create script

--- create_random_collection.py
+++ create_random_collection.py
@@ -1,16 +1,25 @@
-import os, random, sys
+import argparse
+import os
+import random
 
-source = os.path.abspath(sys.argv[1])
-dest = os.path.abspath(sys.argv[2])
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('source', type=str, help='image path')
+parser.add_argument('destination', type=str, help='output path path')
+parser.add_argument('fileextension', type=str, help='file extension')
 
-all_filenames = [fn for fn in os.listdir(source) if fn.split('.')[-1] == 'gif']
-randomized_files = sorted(all_filenames, key = lambda x: random.random())
+args = parser.parse_args()
+
+source = os.path.abspath(args.source)
+dest = os.path.abspath(args.destination)
+file_extension = args.fileextension
+
+all_filenames = [fn for fn in os.listdir(source) if fn.split('.')[-1] == file_extension]
+randomized_files = sorted(all_filenames, key=lambda x: random.random())
 
 current_number = len(os.listdir(dest))
 
 for filename in randomized_files:
-  command = f'mv {source}/{filename} {dest}/{current_number:04d}.gif'
-  print(command)
-  os.system(command)
-  current_number += 1
-  
+    command = f'cp {source}/{filename} {dest}/{current_number:04d}.{file_extension}'
+    print(command)
+    os.system(command)
+    current_number += 1
